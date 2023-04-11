62251264dc30f7ef568c2ef40d1b22c10b141f7c
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2023-04-10 20:31:36 -0400

    Initial WIP

---
+++ Source/RimIgnition/IncidentWorker_rimIgnition.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace RimIgnition
+{
+    public class IncidentWorker_RimIgnition : IncidentWorker
+    {
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            return RimIgnitionUtility.GetIgnitables((Map)parms.target).Any();
+        }
+
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            if (!RimIgnitionUtility.GetIgnitables((Map)parms.target).TryRandomElement(out var result))
+            {
+                return false;
+            }
+            RimIgnitionUtility.TryIgniteFireNear(result);
+            return true;
+        }
+    }
+}
---
+++ Source/RimIgnition/RimIgnitionUtility.cs
@@ -0,0 +1,60 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace RimIgnition
+{
+    public static class RimIgnitionUtility
+    {
+
+        public static IEnumerable<Building> GetIgnitables(Map map)
+        {
+            List<Thing> ignitables = map.listerThings.ThingsOfDef(ThingDefOf.Campfire);
+            ignitables.AddRange(map.listerThings.ThingsOfDef(ThingDefOf.TorchLamp));
+            if (ModsConfig.RoyaltyActive)
+            {
+                ignitables.AddRange(map.listerThings.ThingsOfDef(ThingDefOf.Brazier));
+            }
+            /*
+            if (ModLister.HasActiveModWithName("Vanilla Furniture Expanded"))
+            {
+                ignitables.AddRange(map.listerThings.ThingsOfDef(ThingDefOf.Stone_Campfire));
+            }
+            if (ModLister.HasActiveModWithName("Medieval Overhaul"))
+            {
+                ignitables.AddRange(map.listerThings.ThingsOfDef(ThingDefOf.DankPyon_RusticTorchLamp));
+                ignitables.AddRange(map.listerThings.ThingsOfDef(ThingDefOf.DankPyon_WallTorch));
+            }
+            */
+            for (int i = 0; i < ignitables.Count; i++)
+            {
+                Building building = (Building)ignitables[i];
+                CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                if (refuelComp == null || refuelComp.HasFuel)
+                {
+                    yield return building;
+                }
+            }
+        }
+
+        public static bool TryIgniteFireNear(Building b)
+        {
+            List<IntVec3> tmpCells = new List<IntVec3>();
+            int num = GenRadial.NumCellsInRadius(3f);
+            CellRect startRect = b.OccupiedRect();
+            for (int i = 0; i < num; i++)
+            {
+                IntVec3 intVec = b.Position + GenRadial.RadialPattern[i];
+                if (GenSight.LineOfSight(b.Position, intVec, b.Map, startRect, CellRect.SingleCell(intVec)) && FireUtility.ChanceToStartFireIn(intVec, b.Map) > 0f)
+                {
+                    tmpCells.Add(intVec);
+                }
+            }
+            if (tmpCells.Any())
+            {
+                return FireUtility.TryStartFireIn(tmpCells.RandomElement(), b.Map, Rand.Range(0.1f, 1.75f));
+            }
+            return false;
+        }
+    }
+}

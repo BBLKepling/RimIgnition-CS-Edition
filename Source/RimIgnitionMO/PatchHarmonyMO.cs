4424bfbf2c53bd6e9fb93df839cd5f3a6bcf8047
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2023-04-12 14:58:18 -0400

    More Medieval Overhaul

--- Source/RimIgnitionMO/PatchHarmonyMO.cs
+++ Source/RimIgnitionMO/PatchHarmonyMO.cs
@@ -35,6 +35,66 @@ namespace RimIgnitionMO
                     yield return building;
                 }
             }
+            List<Thing> ignitablesMOWallLamp = map.listerThings.ThingsOfDef(ThingDefOfMO.DankPyon_WallLamp);
+            for (int i = 0; i < ignitablesMOWallLamp.Count; i++)
+            {
+                Building building = (Building)ignitablesMOWallLamp[i];
+                CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                if (refuelComp == null || refuelComp.HasFuel)
+                {
+                    yield return building;
+                }
+            }
+            List<Thing> ignitablesMOLampPost = map.listerThings.ThingsOfDef(ThingDefOfMO.DankPyon_LampPost);
+            for (int i = 0; i < ignitablesMOLampPost.Count; i++)
+            {
+                Building building = (Building)ignitablesMOLampPost[i];
+                CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                if (refuelComp == null || refuelComp.HasFuel)
+                {
+                    yield return building;
+                }
+            }
+            List<Thing> ignitablesMOCandle = map.listerThings.ThingsOfDef(ThingDefOfMO.DankPyon_Candles);
+            for (int i = 0; i < ignitablesMOCandle.Count; i++)
+            {
+                Building building = (Building)ignitablesMOCandle[i];
+                CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                if (refuelComp == null || refuelComp.HasFuel)
+                {
+                    yield return building;
+                }
+            }
+            List<Thing> ignitablesMOCandleWax = map.listerThings.ThingsOfDef(ThingDefOfMO.DankPyon_Candles_Beeswax);
+            for (int i = 0; i < ignitablesMOCandleWax.Count; i++)
+            {
+                Building building = (Building)ignitablesMOCandleWax[i];
+                CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                if (refuelComp == null || refuelComp.HasFuel)
+                {
+                    yield return building;
+                }
+            }
+            List<Thing> ignitablesMOCandleStand = map.listerThings.ThingsOfDef(ThingDefOfMO.DankPyon_CandleStand);
+            for (int i = 0; i < ignitablesMOCandleStand.Count; i++)
+            {
+                Building building = (Building)ignitablesMOCandleStand[i];
+                CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                if (refuelComp == null || refuelComp.HasFuel)
+                {
+                    yield return building;
+                }
+            }
+            List<Thing> ignitablesMORusticLamp = map.listerThings.ThingsOfDef(ThingDefOfMO.DankPyon_RusticLamp);
+            for (int i = 0; i < ignitablesMORusticLamp.Count; i++)
+            {
+                Building building = (Building)ignitablesMORusticLamp[i];
+                CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                if (refuelComp == null || refuelComp.HasFuel)
+                {
+                    yield return building;
+                }
+            }
         }
     }
 }
--- Source/RimIgnitionMO/ThingDefOfMO.cs
+++ Source/RimIgnitionMO/ThingDefOfMO.cs
@@ -8,6 +8,12 @@ namespace RimIgnitionMO
     {
         public static ThingDef DankPyon_RusticTorchLamp;
         public static ThingDef DankPyon_WallTorch;
+        public static ThingDef DankPyon_WallLamp;
+        public static ThingDef DankPyon_LampPost;
+        public static ThingDef DankPyon_Candles;
+        public static ThingDef DankPyon_Candles_Beeswax;
+        public static ThingDef DankPyon_CandleStand;
+        public static ThingDef DankPyon_RusticLamp;
 
         static ThingDefOfMO()
         {

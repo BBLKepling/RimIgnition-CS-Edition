04c0446c64eb23d1c8f34111e4d7d8b9f6e6777f
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2023-04-12 03:49:58 -0400

    Alpha Bees Candles

---
+++ Source/RimIgnitionAlphaBees/HarmonyInitAlphaBees.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace RimIgnitionAlphaBees
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyInitAlphaBees
+    {
+        static HarmonyInitAlphaBees()
+        {
+            Harmony harmonyInstance = new Harmony("BBLKepling.RimIgnition.AlphaBees");
+            harmonyInstance.PatchAll();
+        }
+    }
+}
---
+++ Source/RimIgnitionAlphaBees/ThingDefOfAlphaBees.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+namespace RimIgnitionAlphaBees
+{
+    [DefOf]
+    public class ThingDefOfAlphaBees
+    {
+        public static ThingDef RB_WaxCandle;
+
+        static ThingDefOfAlphaBees()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(ThingDefOfAlphaBees));
+        }
+    }
+}

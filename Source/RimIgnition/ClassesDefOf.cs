fc3c9814b0a89140a8b6c06518020fc75cc937ea
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2023-04-12 21:37:23 -0400

    fckn attributes, how do they work

---
+++ Source/RimIgnition/ClassesDefOf.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimIgnition
+{
+    [DefOf]
+    public class ClassesDefOf
+    {
+        [MayRequireIdeology]
+        public static ThingDef Darktorch;
+        [MayRequireIdeology]
+        public static ThingDef DarktorchFungus;
+        [MayRequireIdeology][MayRequireRoyalty]
+        public static ThingDef DarklightBrazier;
+        [MayRequire("sarg.rimbees")]
+        public static ThingDef RB_WaxCandle;
+        [MayRequire("dankpyon.medieval.overhaul")]
+        public static ThingDef DankPyon_RusticTorchLamp;
+        [MayRequire("dankpyon.medieval.overhaul")]
+        public static ThingDef DankPyon_WallTorch;
+        [MayRequire("dankpyon.medieval.overhaul")]
+        public static ThingDef DankPyon_WallLamp;
+        [MayRequire("dankpyon.medieval.overhaul")]
+        public static ThingDef DankPyon_LampPost;
+        [MayRequire("dankpyon.medieval.overhaul")]
+        public static ThingDef DankPyon_Candles;
+        [MayRequire("dankpyon.medieval.overhaul")]
+        public static ThingDef DankPyon_Candles_Beeswax;
+        [MayRequire("dankpyon.medieval.overhaul")]
+        public static ThingDef DankPyon_CandleStand;
+        [MayRequire("dankpyon.medieval.overhaul")]
+        public static ThingDef DankPyon_RusticLamp;
+        [MayRequire("vanillaexpanded.vfecore")]
+        public static ThingDef Stone_Campfire;
+
+        static ClassesDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(ClassesDefOf));
+        }
+    }
+}
--- Source/RimIgnition/RimIgnitionUtility.cs
+++ Source/RimIgnition/RimIgnitionUtility.cs
@@ -46,6 +46,151 @@ namespace RimIgnition
                     }
                 }
             }
+            if (ModsConfig.IdeologyActive)
+            {
+                List<Thing> ignitablesDarktorch = map.listerThings.ThingsOfDef(ClassesDefOf.Darktorch);
+                for (int i = 0; i < ignitablesDarktorch.Count; i++)
+                {
+                    Building building = (Building)ignitablesDarktorch[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+                List<Thing> ignitablesDarktorchFungus = map.listerThings.ThingsOfDef(ClassesDefOf.DarktorchFungus);
+                for (int i = 0; i < ignitablesDarktorchFungus.Count; i++)
+                {
+                    Building building = (Building)ignitablesDarktorchFungus[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+            }
+            if (ModsConfig.RoyaltyActive && ModsConfig.IdeologyActive)
+            {
+                List<Thing> ignitablesDarklightBrazier = map.listerThings.ThingsOfDef(ClassesDefOf.DarklightBrazier);
+                for (int i = 0; i < ignitablesDarklightBrazier.Count; i++)
+                {
+                    Building building = (Building)ignitablesDarklightBrazier[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+            }
+            if (ModLister.HasActiveModWithName("Alpha Bees"))
+            {
+                List<Thing> ignitablesABCandle = map.listerThings.ThingsOfDef(ClassesDefOf.RB_WaxCandle);
+                for (int i = 0; i < ignitablesABCandle.Count; i++)
+                {
+                    Building building = (Building)ignitablesABCandle[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+            }
+            if (ModLister.HasActiveModWithName("Medieval Overhaul"))
+            {
+                List<Thing> ignitablesMOTorchLamp = map.listerThings.ThingsOfDef(ClassesDefOf.DankPyon_RusticTorchLamp);
+                for (int i = 0; i < ignitablesMOTorchLamp.Count; i++)
+                {
+                    Building building = (Building)ignitablesMOTorchLamp[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+                List<Thing> ignitablesMOWallTorch = map.listerThings.ThingsOfDef(ClassesDefOf.DankPyon_WallTorch);
+                for (int i = 0; i < ignitablesMOWallTorch.Count; i++)
+                {
+                    Building building = (Building)ignitablesMOWallTorch[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+                List<Thing> ignitablesMOWallLamp = map.listerThings.ThingsOfDef(ClassesDefOf.DankPyon_WallLamp);
+                for (int i = 0; i < ignitablesMOWallLamp.Count; i++)
+                {
+                    Building building = (Building)ignitablesMOWallLamp[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+                List<Thing> ignitablesMOLampPost = map.listerThings.ThingsOfDef(ClassesDefOf.DankPyon_LampPost);
+                for (int i = 0; i < ignitablesMOLampPost.Count; i++)
+                {
+                    Building building = (Building)ignitablesMOLampPost[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+                List<Thing> ignitablesMOCandle = map.listerThings.ThingsOfDef(ClassesDefOf.DankPyon_Candles);
+                for (int i = 0; i < ignitablesMOCandle.Count; i++)
+                {
+                    Building building = (Building)ignitablesMOCandle[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+                List<Thing> ignitablesMOCandleWax = map.listerThings.ThingsOfDef(ClassesDefOf.DankPyon_Candles_Beeswax);
+                for (int i = 0; i < ignitablesMOCandleWax.Count; i++)
+                {
+                    Building building = (Building)ignitablesMOCandleWax[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+                List<Thing> ignitablesMOCandleStand = map.listerThings.ThingsOfDef(ClassesDefOf.DankPyon_CandleStand);
+                for (int i = 0; i < ignitablesMOCandleStand.Count; i++)
+                {
+                    Building building = (Building)ignitablesMOCandleStand[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+                List<Thing> ignitablesMORusticLamp = map.listerThings.ThingsOfDef(ClassesDefOf.DankPyon_RusticLamp);
+                for (int i = 0; i < ignitablesMORusticLamp.Count; i++)
+                {
+                    Building building = (Building)ignitablesMORusticLamp[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+            }
+            if (ModLister.HasActiveModWithName("Vanilla Furniture Expanded"))
+            {
+                List<Thing> ignitablesVFECampFire = map.listerThings.ThingsOfDef(ClassesDefOf.Stone_Campfire);
+                for (int i = 0; i < ignitablesVFECampFire.Count; i++)
+                {
+                    Building building = (Building)ignitablesVFECampFire[i];
+                    CompRefuelable refuelComp = building.GetComp<CompRefuelable>();
+                    if (refuelComp == null || refuelComp.HasFuel)
+                    {
+                        yield return building;
+                    }
+                }
+            }
         }
 
         public static bool TryIgniteFireNear(Building culprit)
